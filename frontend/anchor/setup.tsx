36536dbadd0de64cf4ec15171267c0d9f630ab6f
omega000133 <hanzhang.tw@gmail.com>
2024-10-10 08:23:26 -0400

    feat: update anchor provider

--- frontend/anchor/setup.tsx
+++ frontend/anchor/setup.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as anchor from "@coral-xyz/anchor";
 import { AnchorProvider, Program } from "@coral-xyz/anchor";
 import {
     AnchorWallet,
@@ -7,7 +8,15 @@ import {
     useWallet,
 } from "@solana/wallet-adapter-react";
 import { SolanaNftProject, IDL } from "./solana_nft_project";
-import { PublicKey } from "@solana/web3.js";
+import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
+import {
+    findMasterEditionPda,
+    findMetadataPda,
+    mplTokenMetadata,
+} from "@metaplex-foundation/mpl-token-metadata";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { publicKey } from "@metaplex-foundation/umi";
+import { Keypair, PublicKey } from "@solana/web3.js";
 
 export const useAnchorValues = () => {
     const { connection } = useConnection();
@@ -21,10 +30,45 @@ export const useAnchorValues = () => {
     const programId: string = IDL.address;
     const program = new Program<SolanaNftProject>(IDL, provider);
 
+    const umi = createUmi("https://staging-rpc.dev2.eclipsenetwork.xyz")
+        .use(walletAdapterIdentity(wallet))
+        .use(mplTokenMetadata());
+
+    const [collectionPDA] = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from("platinum_collection")],
+        program.programId
+    );
+
+    const mint = Keypair.generate();
+
+    const metadataPDA = findMetadataPda(umi, {
+        mint: publicKey(mint.publicKey),
+    })[0];
+
+    const masterEditionPDA = findMasterEditionPda(umi, {
+        mint: publicKey(mint.publicKey),
+    })[0];
+
+    let collectionMasterEditionPDA = findMasterEditionPda(umi, {
+        mint: publicKey(collectionPDA),
+    })[0];
+
+    let collectionMetadataPDA = findMetadataPda(umi, {
+        mint: publicKey(collectionPDA),
+    })[0];
+
+    const remainingAccounts: Array<any> = [
+        { pubkey: new PublicKey(metadataPDA), isWritable: true, isSigner: false },
+        { pubkey: new PublicKey(masterEditionPDA), isWritable: true, isSigner: false },
+        { pubkey: new PublicKey(collectionMetadataPDA), isWritable: true, isSigner: false },
+        { pubkey: new PublicKey(collectionMasterEditionPDA), isWritable: true, isSigner: false },
+    ];
 
     return {
         program,
         connection,
         programId,
+        mint,
+        remainingAccounts
     };
 };
