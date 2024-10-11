a38882276ad4f0e0f22251960e4340f626f56b42
omega000133 <hanzhang.tw@gmail.com>
2024-10-11 01:28:24 -0400

    feat: update NftMint button with spin action

--- frontend/src/components/ui/MintNFT.tsx
+++ frontend/src/components/ui/MintNFT.tsx
@@ -3,15 +3,20 @@
 import * as React from 'react';
 
 import * as anchor from "@coral-xyz/anchor";
+import { CircularProgress } from '@mui/material';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { useAnchorValues } from '../../../anchor/setup';
 import toast from 'react-hot-toast';
+import { useState } from 'react';
 
 export default function MintNFT() {
     const { program, mint, treasuryWallet, remainingAccounts, connection, provider } = useAnchorValues();
     const { publicKey, connected } = useWallet();
 
+    const [loading, setLoading] = useState(false);
+
     const mintNFT = async () => {
+        setLoading(true);
         try {
             if (!connected) {
                 throw new Error("Wallet not connected");
@@ -65,16 +70,20 @@ export default function MintNFT() {
             toast.success("You have successfully minted an NFT!");
         } catch (err) {
             console.log("ERROR:", err)
+            toast.error("An error occurred while minting the NFT.");
+        } finally {
+            setLoading(false);
         }
     }
 
     return (
         <div className="">
             <button
-                className="bg-pink-400 hover:bg-pink-500 text-white font-bold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105"
+                className="bg-pink-400 hover:bg-pink-500 w-30 text-white font-bold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105"
                 onClick={mintNFT}
+                disabled={loading}
             >
-                Mint NFT
+                {loading ? <CircularProgress size={20} /> : "Mint NFT"}
             </button>
         </div>
     );
