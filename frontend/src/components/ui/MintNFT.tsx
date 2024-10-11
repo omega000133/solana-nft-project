29e9a667f4dffbeff3cda00def06cfd41ef30fa9
omega000133 <hanzhang.tw@gmail.com>
2024-10-10 22:05:36 -0400

    feat: add background color and logo

---
+++ frontend/src/components/ui/MintNFT.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+export default function MintNFT() {
+    return (
+        <div className="">
+            <button
+                className="bg-pink-400 hover:bg-pink-500 text-white font-bold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105"
+            >
+                Mint NFT
+            </button>
+        </div>
+    );
+}
